6abeb5badbc47b7f3d0e458b3f8ad092867fbc0e
ainsley215 <ainsley02121@gmail.com>
2024-02-02 10:44:37 +0700

    Create PerulanganDoWhile.java

---
+++ Minggu 1/PerulanganDoWhile.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package perulangandowhile;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author REDMI
+ */
+public class PerulanganDoWhile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+Scanner input = new Scanner(System.in);
+
+int count = 10;
+do{
+    System.out.println("Hai");
+    count--;
+} while (count > 1);
+    }
+    
+}
